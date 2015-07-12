c6f41fee73a17e3862bab4ed468ed358a918fd64
sophiaduckworth <sophiaduckworth@gmail.com>
2015-07-12 15:39:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,31 @@ var app = express();
 app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", function(request, response){
+    console.log("it works")
     response.sendFile(path.join(__dirname, "pages/index.html"));
 });
 
-app.post('/score', function(request, response){
-    
+app.get("/myGame", function(request, response){
+    console.log("loading")
+    response.sendFile(path.join(__dirname, "pages/example.html"));
 });
+
+    app.post("/score", function(request, response){
+        var name = request.body.fullName;
+        var email = request.body.email;
+        var score = request.body.score;
+
+        var database = csv.createCsvFileWriter("scores.csv", {"flags": "a"});
+        var data = [name,email,score];
+
+        database.writeRecord(data);
+        database.writeStream.end();
+
+        response.send("Thanks " + name + ",your score has been recorded!")
+
+});
+
+
 
 var server = app.listen(8080, function() {
     var host = server.address().address;
